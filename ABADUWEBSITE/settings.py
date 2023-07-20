c416b88d7425f4023f3c142b755f55cbdb310a97
Ronaldyika <buhnyuyronald@gmail.com>
2023-07-20 10:16:33 +0100

    settings modified

--- ABADUWEBSITE/settings.py
+++ ABADUWEBSITE/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4&f%zwi1!=@)^_i50ho$f69q*m9f^^q^i%o!d_2)e1qf0mkx*)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,8 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILE_DIRS = [
-    BASE_DIR / 'static/'
+    os.path.join(BASE_DIR , 'static')
 ]
 
 MEDIA_URL = '/media/'

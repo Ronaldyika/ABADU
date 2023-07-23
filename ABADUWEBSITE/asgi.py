e2bab4bcc3b1da0d8b5b2b15fcbf23a60555aa7e
Ronaldyika <buhnyuyronald@gmail.com>
2023-07-24 00:22:55 +0100

    agi update

--- ABADUWEBSITE/asgi.py
+++ ABADUWEBSITE/asgi.py
@@ -8,9 +8,4 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ABADUWEBSITE.settings')
-
-application = get_asgi_application()

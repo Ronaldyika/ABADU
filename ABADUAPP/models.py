60bcacadfe9c9770b453f9351b72aabf3a8597e5
Ronaldyika <buhnyuyronald@gmail.com>
2023-09-02 13:28:02 +0100

    today

--- ABADUAPP/models.py
+++ ABADUAPP/models.py
@@ -1,27 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
-class Room(models.Model):
-    room_name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.room_name
-class Admininfo(User):
-
-    def __str__(self):
-        return self.username
 
 class Gallery(models.Model):
     title = models.CharField(max_length=20)
     image = models.FileField(upload_to='media')
     description = models.TextField()
     date_posted = models.DateTimeField(auto_now_add='true')
-
-class Message(models.Model):
-    room = models.ForeignKey(Room,on_delete=models.CASCADE, default='')
-    sender = models.CharField(max_length=255)
-    message = models.TextField()
 
 class blogpost(models.Model):
     image = models.ImageField(upload_to='media')
--- ABADUWEBSITE/settings.py
+++ ABADUWEBSITE/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ABADUAPP',
+    'abaduapp',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -141,3 +143,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'admingallery'
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'index'
